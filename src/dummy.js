041295990832be57be5a96d5873937acc2511bd0
frankheine <frankie.heine@gmail.com>
2026-05-27 05:56:59 -0500

    Modified dummy.js and added vercel.json

--- src/dummy.js
+++ src/dummy.js
@@ -1,6 +1,20 @@
 export default {};
 export const connect = async () => ({
   tableNames: async () => [],
-  openTable: async () => ({}),
-  createTable: async () => ({})
+  openTable: async () => ({
+    add: async () => { },
+    vectorSearch: () => ({
+      limit: () => ({
+        toArray: async () => []
+      })
+    })
+  }),
+  createTable: async () => ({
+    add: async () => { },
+    vectorSearch: () => ({
+      limit: () => ({
+        toArray: async () => []
+      })
+    })
+  })
 });
